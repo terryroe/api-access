8a859e659c1ea2add8c963631cd6308daec3e848
Terry Roe <terryroe@gmail.com>
2023-06-14 12:04:01 -0600

    add books array to begin api work

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let books = [
+  {
+    title: 'Great Expectations',
+    authors: [
+      {
+        name: 'Dickens, Charles',
+        birth_year: 1812,
+        death_year: 1870,
+      },
+    ],
+    languages: ['en'],
+    copyright: false,
+    download_count: 15223,
+  },
+  {
+    title: 'The Jungle',
+    authors: [
+      {
+        name: 'Sinclair, Upton',
+        birth_year: 1878,
+        death_year: 1968,
+      },
+    ],
+    languages: ['en'],
+    copyright: false,
+    download_count: 2974,
+  },
+  {
+    title: 'Adventures of Huckleberry Finn',
+    authors: [
+      {
+        name: 'Twain, Mark',
+        birth_year: 1835,
+        death_year: 1910,
+      },
+    ],
+    languages: ['en'],
+    copyright: false,
+    download_count: 12764,
+  },
+];
