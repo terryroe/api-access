adf3342f5deddbdc974103889d8e3dffc5eac770
Terry Roe <terryroe@gmail.com>
2023-06-15 12:03:16 -0600

    add an IIFE to encapsulate the book list

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,80 @@
-let books = [
-  {
-    title: 'Great Expectations',
-    authors: [
-      {
-        name: 'Dickens, Charles',
-        birth_year: 1812,
-        death_year: 1870,
-      },
-    ],
-    languages: ['en'],
-    copyright: false,
-    download_count: 15223,
-  },
-  {
-    title: 'The Jungle',
-    authors: [
-      {
-        name: 'Sinclair, Upton',
-        birth_year: 1878,
-        death_year: 1968,
-      },
-    ],
-    languages: ['en'],
-    copyright: false,
-    download_count: 2974,
-  },
-  {
-    title: 'Adventures of Huckleberry Finn',
-    authors: [
-      {
-        name: 'Twain, Mark',
-        birth_year: 1835,
-        death_year: 1910,
-      },
-    ],
-    languages: ['en'],
-    copyright: false,
-    download_count: 12764,
-  },
-];
+let bookRepository = (function () {
+  let books = [
+    {
+      title: 'Great Expectations',
+      authors: [
+        {
+          name: 'Dickens, Charles',
+          birth_year: 1812,
+          death_year: 1870,
+        },
+      ],
+      languages: ['en'],
+      copyright: false,
+      download_count: 15223,
+    },
+    {
+      title: 'The Jungle',
+      authors: [
+        {
+          name: 'Sinclair, Upton',
+          birth_year: 1878,
+          death_year: 1968,
+        },
+      ],
+      languages: ['en'],
+      copyright: false,
+      download_count: 2974,
+    },
+    {
+      title: 'Adventures of Huckleberry Finn',
+      authors: [
+        {
+          name: 'Twain, Mark',
+          birth_year: 1835,
+          death_year: 1910,
+        },
+      ],
+      languages: ['en'],
+      copyright: false,
+      download_count: 12764,
+    },
+  ];
+
+  // Return the full list of books currently in the list.
+  function getAll() {
+    return books;
+  }
+
+  // Add a book to the list of books.
+  function add(book) {
+    books.push(book);
+  }
+
+  return {
+    getAll,
+    add,
+  };
+})();
+
+// Test adding a book to the list. This is just to see if the add() method works
+bookRepository.add({
+  title: 'The Keep',
+  authors: [
+    {
+      name: 'Wilson, F. Paul',
+      birth_year: 1946,
+      death_year: null,
+    },
+  ],
+  languages: ['en'],
+  copyright: true,
+  download_count: 19000,
+});
 
 // Loop over all the books beginning at the first (0) element and ending with
 // the last (books.length) element.
-books.forEach(function (book) {
+bookRepository.getAll().forEach(function (book) {
   // `document.write()` is a "violation" according to the developer console, but
   // it's ok for the purposes of this demonstration.
   document.write('<h1>' + book.title + '</h1>');
